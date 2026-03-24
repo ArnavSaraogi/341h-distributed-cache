ad657476fcace21a204389f84f22dc5a82779ca1
arnsar <arnavsaraogi1@gmail.com>
2026-03-23 21:09:18 -0500

    refactored client a bit

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,22 +10,13 @@ import (
 	"time"
 )
 
+var ring *cache_ring.CacheRing
+
 func main() {
-	ticker := time.NewTicker(10 * time.Second)
-	defer ticker.Stop()
-	var ips []string
+	ring = cache_ring.NewRing()
 
-	go func() {
-		for range ticker.C {
-			getIps(&ips)
-		}
-	}()
+	go getIps() // goroutine to get the ips, runs continously
 
-	// add caches to the ring
-	ring := cache_ring.NewRing()
-	for i := 0; i < len(ips); i++ {
-		ring.AddIP(ips[i])
-	}
 	request := "GET sanjiv"
 	/*
 		1. getc correct cache ip for incoming req
@@ -43,17 +34,30 @@ func main() {
 	}
 }
 
-func getIps(ips *[]string) {
-	res, err := http.Get("http://localhost:8080/ips") // storing config service on same machine for now
-	if err != nil {
-		panic(err)
-	}
-	body, err := io.ReadAll(res.Body)
-	if err != nil {
-		panic(err)
-	}
-	err = json.Unmarshal(body, ips)
-	if err != nil {
-		panic(err)
+func getIps() {
+	ticker := time.NewTicker(10 * time.Second)
+	defer ticker.Stop()
+
+	for range ticker.C {
+		ips := []string{}
+
+		// get updated list from config service
+		res, err := http.Get("http://localhost:8080/ips") // storing config service on same machine for now
+		if err != nil {
+			panic(err)
+		}
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			panic(err)
+		}
+		err = json.Unmarshal(body, &ips) // storing returned list in ips
+		if err != nil {
+			panic(err)
+		}
+
+		// add caches to the ring
+		for i := 0; i < len(ips); i++ {
+			ring.AddIP((ips)[i])
+		}
 	}
 }
